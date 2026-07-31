607f6ddab32a8a7caac58f1dbbd8101ecb43e323
Your Name <your@email.com>
2026-07-31 17:31:34 +0100

    Initial commit

---
+++ android/app/src/main/java/com/anshif/auraalaram/MainActivity.java
@@ -0,0 +1,5 @@
+package com.anshif.auraalaram;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anshif.auraalaram',
+  appName: 'AuraAlaram',
+  webDir: 'dist'
+};
+
+export default config;
