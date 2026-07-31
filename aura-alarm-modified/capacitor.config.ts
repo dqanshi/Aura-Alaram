01dd299a9a8cb6f7b25c047e0cf0b5793da7e58a
Your Name <your@email.com>
2026-07-31 19:25:20 +0100

    Apply updated Aura Alarm project

--- aura-alarm-modified/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.anshif.auraalaram',
-  appName: 'AuraAlaram',
-  webDir: 'dist'
-};
-
-export default config;
